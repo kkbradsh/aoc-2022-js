4ffd207cd72c771f953fd7cf83187501b13f1748
kkbradsh <kkbradsh@gmail.com>
2022-12-02 22:22:22 -0800

    Remove unused function

--- src/03/02/index.js
+++ src/03/02/index.js
@@ -1,37 +1,5 @@
 import { readTextFile } from '../../utils/file';
 
-export const save = async (file) => {
-  const input = await readTextFile(file);
-  let priorities = [];
-  for (let i = 0; i < input.length; i++) {
-    // Get compartments
-    const compartment1 = input[i].substring(0, input[i].length / 2);
-    const compartment2 = input[i].substring(input[i].length / 2);
-    // Find priorities
-    let common12 = '';
-    for (let j = 0; j < compartment1.length; j++) {
-      if (
-        compartment2.includes(compartment1[j]) &&
-        !common12.includes(compartment1[j])
-      )
-        common12 = common12 + compartment1[j];
-    }
-    let common = '';
-
-    priorities.push(common);
-  }
-  // Sum priorities
-  // ascii value of 'A' is 65, priorities of A-Z is 27 - 52
-  // ascii value of 'a' is 97, a-z is 1 - 26
-  return priorities.reduce((sum, item) => {
-    if (item == item.toLowerCase()) {
-      return sum + item.charCodeAt(0) - 97 + 1;
-    } else {
-      return sum + item.charCodeAt(0) - 65 + 27;
-    }
-  }, 0);
-};
-
 export const process = async (file) => {
   const input = await readTextFile(file);
   let priorities = [];
