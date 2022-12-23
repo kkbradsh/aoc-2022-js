232b4bb74e2c015e1112c6e3fd9d6e2ce623cd5c
kkbradsh <kkbradsh@gmail.com>
2022-12-23 07:36:20 -0800

    Add day 23

---
+++ src/23/01/index.js
@@ -0,0 +1,112 @@
+import { readTextFile } from '../../utils/file';
+
+const ROUNDS = 10;
+
+const POSITIONS = new Map([
+  ['N', [-1, 0]],
+  ['NE', [-1, 1]],
+  ['E', [0, 1]],
+  ['SE', [1, 1]],
+  ['S', [1, 0]],
+  ['SW', [1, -1]],
+  ['W', [0, -1]],
+  ['NW', [-1, -1]],
+]);
+
+const DIRECTIONS = [
+  ['N', ['N', 'NE', 'NW']],
+  ['S', ['S', 'SE', 'SW']],
+  ['W', ['W', 'NW', 'SW']],
+  ['E', ['E', 'NE', 'SE']],
+];
+
+const move = (map) => {
+  let round = 0;
+  let needMove = true;
+  let direction = 0;
+  while (needMove && round < ROUNDS) {
+    // Propose Moves
+    const proposedMoves = [];
+    for (let key of map.keys()) {
+      const elf = key;
+      const [row, col] = elf.split(',').map((item) => Number(item));
+      // Need to move? Elves in one of 8 directions.
+      const relElves = [...POSITIONS].reduce((arr, [key, [relRow, relCol]]) => {
+        if (map.has(`${row + relRow},${col + relCol}`)) {
+          arr.push(key);
+        }
+        return arr;
+      }, []);
+      // Propose move (starting from preferred direction, but checking all directions)
+      if (relElves.length === 0) continue;
+      for (let i = 0; i < DIRECTIONS.length; i++) {
+        const next = (direction + i) % DIRECTIONS.length;
+        const [nextDirection, nextPositions] = DIRECTIONS[next];
+        const foundElves = nextPositions.filter((relElf) => {
+          return relElves.includes(relElf);
+        });
+        if (foundElves.length === 0) {
+          const [relRow, relCol] = POSITIONS.get(nextDirection);
+          proposedMoves.push([
+            row,
+            col,
+            row + relRow,
+            col + relCol,
+            nextDirection,
+          ]);
+          break;
+        }
+      }
+    }
+    // Remove duplicate moves
+    const destinations = proposedMoves.map(
+      ([_fromRow, _fromCol, toRow, toCol, _direction]) => [toRow, toCol]
+    );
+    const finalMoves = proposedMoves.filter((move) => {
+      const [_fromRow, _fromCol, toRow, toCol, _direction] = move;
+      const foundDestination = destinations.filter(([row, col]) => {
+        return row === toRow && col === toCol;
+      });
+      if (foundDestination.length === 1) return true;
+    });
+    // Move elves
+    if (finalMoves.length === 0) needMove = false;
+    else {
+      finalMoves.forEach(([fromRow, fromCol, toRow, toCol, _direction]) => {
+        map.delete(`${fromRow},${fromCol}`);
+        map.set(`${toRow},${toCol}`);
+      });
+    }
+    round = round + 1;
+    // Choose next preferred direction
+    direction = (direction + 1) % DIRECTIONS.length;
+  }
+};
+
+const parseInput = (input) => {
+  const map = new Map();
+  for (let row = 0; row < input.length; row++) {
+    const cols = input[row].split('');
+    for (let col = 0; col < cols.length; col++) {
+      if (input[row][col] === '#') map.set(`${row + 1},${col + 1}`);
+    }
+  }
+  return map;
+};
+
+export const process = async (file) => {
+  const input = await readTextFile(file);
+  const map = parseInput(input);
+  move(map);
+  // Return: find the rectangle that contains the elves and count how many spaces do not contain elves
+  let minRow, maxRow, minCol, maxCol;
+  for (let key of map.keys()) {
+    const [row, col] = key.split(',');
+    minRow = Math.min(minRow !== undefined ? minRow : row, row);
+    maxRow = Math.max(maxRow !== undefined ? maxRow : row, row);
+    minCol = Math.min(minCol !== undefined ? minCol : col, col);
+    maxCol = Math.max(maxCol !== undefined ? maxCol : col, col);
+  }
+  const positions = (maxRow - minRow + 1) * (maxCol - minCol + 1);
+  return positions - map.size;
+};
